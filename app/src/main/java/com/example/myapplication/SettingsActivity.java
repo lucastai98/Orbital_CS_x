4e51a992dc72a634567cda671263df6c2ba1ad29
Lucas Tai <lucastai98@hotmail.com>
2020-06-18 23:10:58 +0800

    test

--- app/src/main/java/com/example/myapplication/SettingsActivity.java
+++ app/src/main/java/com/example/myapplication/SettingsActivity.java
@@ -2,14 +2,36 @@ package com.example.myapplication;
 
 import android.os.Build;
 import android.os.Bundle;
+import android.renderscript.Sampler;
+import android.widget.Button;
+import android.widget.EditText;
 
 import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+import com.squareup.picasso.Picasso;
+
+import de.hdodenhof.circleimageview.CircleImageView;
+
 public class SettingsActivity extends AppCompatActivity {
 
     private Toolbar mToolbar;
+
+    private EditText userName;
+    private Button UpdateAccountSettingsButton;
+    private CircleImageView userProfImage;
+
+    private DatabaseReference SettingsUserRef;
+    private FirebaseAuth mAuth;
+
+    private String currentUserId;
 
     @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
     @Override
@@ -17,9 +39,36 @@ public class SettingsActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_settings);
 
+        mAuth = FirebaseAuth.getInstance();
+        currentUserId = mAuth.getCurrentUser().getUid();
+        SettingsUserRef = FirebaseDatabase.getInstance().getReference().child("Users");
+
         mToolbar = (Toolbar) findViewById(R.id.settings_toolbar);
         setSupportActionBar(mToolbar);
         getSupportActionBar().setTitle("Account Settings");
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        userName = (EditText) findViewById(R.id.settings_name);
+        userProfImage = (CircleImageView) findViewById(R.id.settings_profile_image);
+        UpdateAccountSettingsButton = (Button) findViewById(R.id.update_account_settings_button);
+
+        SettingsUserRef.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                if (dataSnapshot.exists()){
+                    String myProfileImage = dataSnapshot.child("profileimage").getValue().toString();
+                    String myUserName = dataSnapshot.child("fullname").getValue().toString();
+
+                    Picasso.get().load(myProfileImage).placeholder(R.drawable.profile).into(userProfImage);
+
+                    userName.setText(myUserName);
+                }
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+
+            }
+        })
     }
 }
